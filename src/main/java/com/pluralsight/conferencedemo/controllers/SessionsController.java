7f7c8014524d86a283de170b64d8bb8549d11e6a
akash_garg <akashgarg3a@gmail.com>
2021-12-28 01:06:12 +0530

    Added session controller

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -0,0 +1,50 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Session;
+import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/vi/sessions")
+public class SessionsController {
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list() {
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable Long id) {
+        return sessionRepository.getById(id);
+    }
+
+    @PostMapping
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        // We need to add logic for children records before deleting
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+        // As it update we except all the value that are passed
+        // TODO: Add validation that all the parameters are passed, except return 400 bad payload
+
+        Session existingSession = sessionRepository.getById(id);
+        // used for copy the data
+        BeanUtils.copyProperties(session, existingSession,"session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
+}
